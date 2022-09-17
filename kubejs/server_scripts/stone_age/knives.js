e4373edccdd12a6d45af65667065d774f0ca5421
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2022-09-17 14:47:16 +0200

    Progression for plant fibers and plant string

---
+++ kubejs/server_scripts/stone_age/knives.js
@@ -0,0 +1,24 @@
+function removeRecipes(event, recipeIds) {
+  recipeIds.forEach(id => {
+    event.remove({
+        id: id
+    });
+  });
+}
+
+onEvent('recipes', event => {
+  removeRecipes(event, [
+    'croptopia:knife',
+    'farmersdelight:flint_knife',
+    'farmersdelight:iron_knife',
+    'farmersdelight:golden_knife',
+    'farmersdelight:diamond_knife',
+    'farmersdelight:netherite_knife_smithing',
+  ]);
+
+  removeRecipes(event, [
+    'notreepunching:plant_string',
+  ]);
+
+  event.remove({output: 'notreepunching:plant_fiber'})
+});
--- kubejs/server_scripts/stone_age/tags.js
+++ kubejs/server_scripts/stone_age/tags.js
@@ -1,4 +1,28 @@
 onEvent('item.tags', event => {
-	event.get('c:tools/knives').add('notreepunching:flint_knife')
-	event.get('c:hoes').add('notreepunching:flint_hoe')
-})
+	event.get('c:knives')
+    .add('notreepunching:flint_knife')
+    .add('notreepunching:iron_knife')
+    .add('notreepunching:gold_knife')
+    .add('notreepunching:diamond_knife')
+    .add('notreepunching:netherite_knife');
+
+	event.get('c:tools/knives')
+    .add('notreepunching:flint_knife')
+    .add('notreepunching:iron_knife')
+    .add('notreepunching:gold_knife')
+    .add('notreepunching:diamond_knife')
+    .add('notreepunching:netherite_knife');
+
+  event.get('farmersdelight:straw_harvesters')
+    .add('notreepunching:flint_knife')
+    .add('notreepunching:iron_knife')
+    .add('notreepunching:gold_knife')
+    .add('notreepunching:diamond_knife')
+    .add('notreepunching:netherite_knife');
+  
+  event.get('minecraft:piglin_loved')
+    .add('notreepunching:gold_knife');
+  
+	event.get('c:hoes')
+    .add('notreepunching:flint_hoe');
+});
