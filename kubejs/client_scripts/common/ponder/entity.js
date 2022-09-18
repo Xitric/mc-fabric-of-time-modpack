d5b4a519af88ebcae31cb1259635c18b45517573
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2022-09-18 12:21:30 +0200

    Extract ponder entity methods to common

---
+++ kubejs/client_scripts/common/ponder/entity.js
@@ -0,0 +1,52 @@
+function ponderCreatePlayerEntity(scene, position, rotation) {
+  const playerLink = scene.world.createEntity("minecraft:skeleton", position);
+  scene.world.modifyEntity(playerLink, (player) => {
+    player.setItemSlot('HEAD', 'minecraft:player_head');
+    player.setItemSlot('CHEST', 'minecraft:leather_chestplate');
+    player.setItemSlot('LEGS', 'minecraft:leather_leggings');
+    player.setItemSlot('FEET', 'minecraft:leather_boots');
+
+    if (rotation) {
+      player.setYRot(rotation);
+      player.setYBodyRot(rotation);
+      player.setYHeadRot(rotation);
+    }
+  });
+
+  return playerLink;
+}
+
+function ponderEntityEquipItem(scene, entityLink, item) {
+  scene.world.modifyEntity(entityLink, (e) => {
+    e.setItemInHand('MAIN_HAND', item);
+  });
+}
+
+function ponderEntityUnequipItem(scene, entityLink) {
+  scene.world.modifyEntity(entityLink, (e) => {
+    e.setItemInHand('MAIN_HAND', 'minecraft:air');
+  });
+}
+
+function ponderEntityUseItem(scene, entityLink) {
+  scene.world.modifyEntity(entityLink, (e) => {
+    e.swing('MAIN_HAND');
+  });
+}
+
+function ponderEntityWalk(scene, entityLink, movementVector, ticks) {
+  let dx = movementVector[0] / ticks;
+  let dy = movementVector[1] / ticks;
+  let dz = movementVector[2] / ticks;
+
+  for (let i = 0; i < ticks; i++) {
+    scene.world.modifyEntity(entityLink, (e) => {
+      e.move('SELF', [dx, dy, dz]);
+    });
+    scene.idle(1);
+  }
+
+  scene.world.modifyEntity(entityLink, (e) => {
+    e.moveTo(e.getPosition(1));
+  });
+}
--- kubejs/client_scripts/stone_age/ponder/flint_knapping.js
+++ kubejs/client_scripts/stone_age/ponder/flint_knapping.js
@@ -1,56 +1,3 @@
-function createPlayer(scene, position, rotation) {
-  const playerLink = scene.world.createEntity("minecraft:skeleton", position);
-  scene.world.modifyEntity(playerLink, (player) => {
-    player.setItemSlot('HEAD', 'minecraft:player_head');
-    player.setItemSlot('CHEST', 'minecraft:leather_chestplate');
-    player.setItemSlot('LEGS', 'minecraft:leather_leggings');
-    player.setItemSlot('FEET', 'minecraft:leather_boots');
-
-    if (rotation) {
-      player.setYRot(rotation);
-      player.setYBodyRot(rotation);
-      player.setYHeadRot(rotation);
-    }
-  });
-
-  return playerLink;
-}
-
-function equipItem(scene, entityLink, item) {
-  scene.world.modifyEntity(entityLink, (e) => {
-    e.setItemInHand('MAIN_HAND', item);
-  });
-}
-
-function unequipItem(scene, entityLink) {
-  scene.world.modifyEntity(entityLink, (e) => {
-    e.setItemInHand('MAIN_HAND', 'minecraft:air');
-  });
-}
-
-function useItem(scene, entityLink) {
-  scene.world.modifyEntity(entityLink, (e) => {
-    e.swing('MAIN_HAND');
-  });
-}
-
-function walkEntity(scene, entityLink, movementVector, ticks) {
-  let dx = movementVector[0] / ticks;
-  let dy = movementVector[1] / ticks;
-  let dz = movementVector[2] / ticks;
-
-  for (let i = 0; i < ticks; i++) {
-    scene.world.modifyEntity(entityLink, (e) => {
-      e.move('SELF', [dx, dy, dz]);
-    });
-    scene.idle(1);
-  }
-
-  scene.world.modifyEntity(entityLink, (e) => {
-    e.moveTo(e.getPosition(1));
-  });
-}
-
 onEvent('ponder.registry', (event) => {
   event.create('minecraft:flint')
     .scene('flint_knapping_scene', 'Knapping flint', (scene, util) => {
@@ -63,11 +10,11 @@ onEvent('ponder.registry', (event) => {
       scene.showStructure();
 
       // Move in player with flint
-      const playerLink = createPlayer(scene, playerSpawn, 90);
-      equipItem(scene, playerLink, 'minecraft:flint');
+      const playerLink = ponderCreatePlayerEntity(scene, playerSpawn, 90);
+      ponderEntityEquipItem(scene, playerLink, 'minecraft:flint');
 
       scene.idle(10);
-      walkEntity(scene, playerLink, [-1, 0, 0], 20);
+      ponderEntityWalk(scene, playerLink, [-1, 0, 0], 20);
 
       // Present setting
       scene.idle(10);
@@ -81,23 +28,23 @@ onEvent('ponder.registry', (event) => {
 
       // Knapping fails
       scene.addKeyframe();
-      useItem(scene, playerLink);
+      ponderEntityUseItem(scene, playerLink);
       scene.idle(5);
-      unequipItem(scene, playerLink);
+      ponderEntityUnequipItem(scene, playerLink);
 
       scene.idle(10);
       scene.text(40, 'Sometimes knapping will fail, destroying the flint in the process', rockTop)
         .placeNearTarget();
       scene.idle(50);
 
-      equipItem(scene, playerLink, 'minecraft:flint');
+      ponderEntityEquipItem(scene, playerLink, 'minecraft:flint');
       scene.idle(20);
 
       // Knapping succeeds
       scene.addKeyframe();
-      useItem(scene, playerLink);
+      ponderEntityUseItem(scene, playerLink);
       scene.idle(5);
-      unequipItem(scene, playerLink);
+      ponderEntityUnequipItem(scene, playerLink);
       scene.world.createItemEntity(rockTop.add(0, 0, 0), util.vector.of(-0.05, 0.2, -0.04), "notreepunching:flint_shard");
 
       scene.idle(10);
